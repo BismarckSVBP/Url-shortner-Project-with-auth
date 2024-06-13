ae2a5fa94644ce2995fb10b73c1bb9eb79483c6c
BismarckSVBP <163289874+BismarckSVBP@users.noreply.github.com>
2024-06-14 00:14:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const homepage = require("./routes/homepage");
 
 
 const port = 10001; 
-connectToMongoDB("mongodb+srv://abhay:abhay@cluster0.nmhaanc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() =>
+connectToMongoDB("mongodb+srv://sahil:sahil@cluster0.nshlde2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() =>
   console.log("MongoDb connected")
 );
 
