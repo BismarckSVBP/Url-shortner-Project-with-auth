d0295d91ef1bef252dd1acd45cc9b974cfe7adf4
BismarckSVBP <163289874+BismarckSVBP@users.noreply.github.com>
2024-06-13 23:27:21 +0530

    Update signup.js

--- models/signup.js
+++ models/signup.js
@@ -19,6 +19,6 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("user", userSchema);
+const User = mongoose.model("userurl", userSchema);
 
 module.exports = User;
